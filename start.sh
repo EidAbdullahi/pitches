c1a8b1758b087ae0b78e28fe5264ed85ebbb375b
EidAbdullahi <eidabdullahi10@gmail.com>
2020-12-08 13:16:11 +0300

    removed start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY='T0717062455T'
-export DATABASE_URL='postgresql+psycopg2://access:Access@localhost/pitches'
-export MAIL_USERNAME='eidabdullahi10@gmail.com'
-export MAIL_PASSWORD='##########'
-
-python3.8 manage.py server
