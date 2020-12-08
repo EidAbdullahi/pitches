0cbd6d26f86f4f42694e8e1135263b3f4ab70a16
EidAbdullahi <eidabdullahi10@gmail.com>
2020-12-08 12:30:50 +0300

    commit all changes.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='T0717062455T'
 export DATABASE_URL='postgresql+psycopg2://access:Access@localhost/pitches'
 export MAIL_USERNAME='eidabdullahi10@gmail.com'
-export MAIL_PASSWORD='0717062455e'
+export MAIL_PASSWORD='#############'
 
 python3.8 manage.py server
